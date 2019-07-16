627725d20d8534264e06fa21a7d60f3a0140b537
yonny huarancca ñaupari <40847911+yonnyhuaranccaUNSCH27@users.noreply.github.com>
2019-07-16 00:42:48 -0500

    mi proyecto

---
+++ divisimabd.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-07-2019 a las 06:28:09
+-- Versión del servidor: 10.1.40-MariaDB
+-- Versión de PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `divisimabd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) NOT NULL,
+  `estado` int(11) NOT NULL,
+  `idedentificacion` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idcategoria`, `estado`, `idedentificacion`, `nombre`) VALUES
+(1, 1, 0, 'mujer'),
+(2, 1, 0, 'pero'),
+(3, 1, 0, 'joyeria'),
+(4, 1, 0, 'zapatos'),
+(5, 1, 0, 'paginas'),
+(6, 1, 4, 'zapatillas'),
+(7, 1, 4, 'sandalias'),
+(8, 1, 4, 'zapatos formales'),
+(9, 1, 4, 'botas'),
+(10, 1, 4, 'chancletas'),
+(11, 1, 5, 'paginas de producto'),
+(12, 1, 5, 'paginas de categoria'),
+(13, 1, 5, 'pagina de carro'),
+(14, 1, 5, 'paginas de pago'),
+(15, 1, 5, 'pagina de contacto');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `color`
+--
+
+CREATE TABLE `color` (
+  `idcolor` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marca`
+--
+
+CREATE TABLE `marca` (
+  `idmarca` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idproducto` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `imagen` varchar(80) NOT NULL,
+  `caracteristica` tinyint(4) NOT NULL,
+  `idcategoria` int(11) NOT NULL,
+  `idcolor` int(11) NOT NULL,
+  `idmarca` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idcategoria`);
+
+--
+-- Indices de la tabla `color`
+--
+ALTER TABLE `color`
+  ADD PRIMARY KEY (`idcolor`);
+
+--
+-- Indices de la tabla `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`idmarca`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD UNIQUE KEY `idcategoria` (`idcategoria`,`idcolor`,`idmarca`),
+  ADD KEY `idcolor` (`idcolor`),
+  ADD KEY `idmarca` (`idmarca`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`),
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`idcolor`) REFERENCES `color` (`idcolor`),
+  ADD CONSTRAINT `producto_ibfk_3` FOREIGN KEY (`idmarca`) REFERENCES `marca` (`idmarca`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
